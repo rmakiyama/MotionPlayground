22c53ddbc45d222a8471ed565975cafb1a858c9a
rmakiyama <rmakiyama.io@gmail.com>
2019-11-09 14:42:30 +0900

    add fragment

--- app/src/main/java/com/rmakiyama/motionplayground/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/motionplayground/MainActivity.kt
@@ -2,11 +2,23 @@ package com.rmakiyama.motionplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commitNow
+import com.rmakiyama.motionplayground.databinding.ActivityMainBinding
+import com.rmakiyama.motionplayground.ui.playgroundlist.PlaygroundListFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commitNow {
+                replace(R.id.container, PlaygroundListFragment())
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/rmakiyama/motionplayground/ui/playgroundlist/PlaygroundListViewModel.kt
@@ -0,0 +1,7 @@
+package com.rmakiyama.motionplayground.ui.playgroundlist
+
+import androidx.lifecycle.ViewModel
+
+class PlaygroundListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
